de1151cfa163490276b04b2c115be3bf56dd2d11
srinivas <srinivashyd472@gmail.com>
2024-12-31 18:56:50 +0530

    code changes

--- script.sh
+++ script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-for i in {1..10}
-do 
-if {$i % 2 -eq 0}
-then 
-echo " $i is even"
-else
-echo "$i is odd"
-fi
+for i in {1..10}; do
+    if {$i % 2 -eq 0}; then
+        echo " $i is even"
+    else
+        echo "$i is odd"
+    fi
+done
