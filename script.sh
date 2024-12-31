bff7dfd4f2ac9e8f9df015a677990550bcdcfc48
srinivas <srinivashyd472@gmail.com>
2024-12-31 19:15:57 +0530

    code changes

--- script.sh
+++ script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 for I in {1..10}; do
-    if [$(expr $I % 2) -eq 0]; then
-        echo " $I is even"
-    else
-        echo "$I is odd"
+    if [ $(expr $I % 2) -eq 0 ]; then
+        echo " $I is even "
     fi
 done
